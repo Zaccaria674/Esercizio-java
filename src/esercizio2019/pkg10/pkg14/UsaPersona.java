404044927e8d3a785407b1a757ede7e341ddc7a1
casinoo <casinoo@ferrara>
2019-10-14 18:07:06 +0200

    commit "Dichiarazione un'altra classe"

---
+++ src/esercizio2019/pkg10/pkg14/UsaPersona.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizio2019.pkg10.pkg14;
+
+/**
+ *
+ * @author casinoo
+ */
+public class UsaPersona  {
+   private String persona1;
+   private String persona2;
+}
